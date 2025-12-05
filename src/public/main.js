43d507abd5653f8ca7a699a08a327c4f51af9132
Auriban <ainhizeub1@gmail.com>
2025-12-05 09:46:40 +0100

    ADD: css y js main

---
+++ src/public/main.js
@@ -0,0 +1,21 @@
+const ham = document.getElementById("hamburger");
+const nav = document.getElementById("navMenu");
+
+ham.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
+
+
+const searchInput = document.getElementById("searchInput");
+const grid = document.getElementById("pelisGrid");
+
+searchInput.addEventListener("input", () => {
+    const term = searchInput.value.toLowerCase();
+    const cards = grid.querySelectorAll(".peli-card");
+
+    cards.forEach(card => {
+        const title = card.querySelector("p").textContent.toLowerCase();
+        card.style.display = title.includes(term) ? "block" : "none";
+    });
+});
+
